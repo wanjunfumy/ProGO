37ee4bf167a3fb461d1fbfeb0043f6957cdd0302
ProGo <258276264@qq.com>
2020-05-06 22:04:10 +0800

    A real project starting! ---- 1、启动路由，后面来的填坑

--- src/CNGo/main.go
+++ src/CNGo/main.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	handles "CNGo/routes"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
 
+var router *httprouter.Router
 
 func main() {
-	
+	router = httprouter.New()
+	for _, v := range handles.Cells {
+		v.Init(router)
+	}
+	log.Fatal(http.ListenAndServe(":8080", router)) // 用了Fatal 就不需要处理err，Fatal中已经有 os.Exit(1)
 }
